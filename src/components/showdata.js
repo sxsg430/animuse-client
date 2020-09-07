b2b5bb83be8b2f25178345691e8fc6598b19824f
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-07 14:27:51 +1000

    Add initial code for getting spotify code from API

--- src/components/showdata.js
+++ src/components/showdata.js
@@ -32,6 +32,7 @@ export class Showdata extends Component {
         } else {
             return(
                 <div>
+                    <iframe src={"https://open.spotify.com/embed/track/" + this.state.song.replace('spotify:track:', '')}  width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                     <h1>Hello {this.state.song}</h1>
                 </div>
             );
@@ -39,7 +40,8 @@ export class Showdata extends Component {
         
     }
     async fetchSongInfo() {
-        const response = await fetch('http://localhost:3000/spotifyreq/Futakotome');
+        let songname = ''; // TODO: Implement
+        const response = await fetch('http://localhost:3000/spotifyreq/' + songname);
         const data = await response.json();
         this.setState({song: data});
     }
