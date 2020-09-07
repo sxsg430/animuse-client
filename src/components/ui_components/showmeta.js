2a95610934b0b344b75546c6a58ee980ed717e9b
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-07 18:53:15 +1000

    Code Cleanup

--- src/components/ui_components/showmeta.js
+++ src/components/ui_components/showmeta.js
@@ -17,26 +17,20 @@ export class ShowMeta extends Component {
         this.setState({ isOpen: !this.state.isOpen })
     }
     render() {
-        //<h1>{this.props.showdata.showInfo.title}</h1>
         return (
             <div>
-                        <Card>
-                            <CardImg top style={{width: '225px', height: '338px', display: 'block', margin: 'auto', textAlign: 'center', justifyContent: 'center', alignItems: 'center'}} src={this.props.showdata.showInfo.cover} />
-                            <CardTitle><h3 style={{textAlign: 'center'}}>{this.props.showdata.showInfo.title}</h3></CardTitle>
-                            <CardSubtitle><h6 style={{textAlign: 'center'}}>{this.props.showdata.showInfo.airdate}</h6></CardSubtitle>
-                            <CardBody>
-                                <p style={{wordWrap: 'break-word', fontSize: '12px'}} ><b>Description:</b> {this.props.showdata.showInfo.description}
-                                <br /><b>Media Type: </b> {this.props.showdata.showInfo.media_type}
-                                <br /><b>Origin: </b> {this.props.showdata.showInfo.origin}
-                                <br /><b>Episode Count: </b> {this.props.showdata.showInfo.epcount}
-
-                                
-                                
-                                
-                                </p>
-                            </CardBody>
-                        </Card>
-                
+                <Card style={{backgroundColor: '#d9d9d9'}}>
+                    <CardImg top style={{width: '225px', height: '338px', display: 'block', margin: 'auto', textAlign: 'center', justifyContent: 'center', alignItems: 'center'}} src={this.props.showdata.showInfo.cover} />
+                    <CardTitle><h3 style={{textAlign: 'center'}}>{this.props.showdata.showInfo.title}</h3></CardTitle>
+                    <CardSubtitle><h6 style={{textAlign: 'center'}}>{this.props.showdata.showInfo.airdate}</h6></CardSubtitle>
+                    <CardBody>
+                        <p style={{wordWrap: 'break-word', fontSize: '12px'}} ><b>Description:</b> {this.props.showdata.showInfo.description}
+                            <br /><b>Media Type: </b> {this.props.showdata.showInfo.media_type}
+                            <br /><b>Origin: </b> {this.props.showdata.showInfo.origin}
+                            <br /><b>Episode Count: </b> {this.props.showdata.showInfo.epcount}
+                        </p>
+                    </CardBody>
+                </Card>
             </div>
         )
     }
