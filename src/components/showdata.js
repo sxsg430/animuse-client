c12710ccd76c22fd0bb098aafefd8db4d7bc6f99
sxsg430 <872597+sxsg430@users.noreply.github.com>
2020-09-13 18:02:03 +1000

    Add placeholder loading grapic

--- src/components/showdata.js
+++ src/components/showdata.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Container, Row, Col,} from 'reactstrap';
+import {Collapse, Spinner, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Container, Row, Col,} from 'reactstrap';
 import ShowMeta from './ui_components/showmeta';
 
 
@@ -17,7 +17,12 @@ export class Showdata extends Component {
     // TODO: Fix CORS
     render() {
         if (!this.state.mal) {
-            return <div />
+            return(
+                <div>
+                    <Spinner color="primary" /><br />
+                    <p className="text-primary">Loading...</p>
+                </div>
+            )
         } else {
             // <iframe src={"https://open.spotify.com/embed/track/" + this.state.song.replace('spotify:track:', '')}  width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             return(
